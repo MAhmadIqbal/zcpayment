b6204fef85081e800ae8f10c2f9b8add315b959f
Ahmadpu <ahmad1.ngxoft@gmail.com>
2021-09-22 19:02:41 +0500

    finalize

--- route/route.js
+++ route/route.js
@@ -4,8 +4,21 @@ const router = express.Router()
 const util = require('../util')        
     //-------Endpoints-Starts-------------------------
 // just locally have file and get request will give response back with file to client
-router.get('/file1/:index',util.fileSend)
+const validation=async function(req,res,next){
+    const indexvalue = parseInt(req.params.index)
+    console.log("index value:",typeof indexvalue)
+        if(indexvalue == 0){
+            console.log('Error due to 0 value')
+            res.status(401).send('router must not equal to 0 in value')
+        }else if(indexvalue > 50000){
+            console.log('Error due to exceeding value')
+            res.status(401).send('route must have value within 50000 in last')    
+        }else{
+            next();     
+        }
+}
+router.get('/file1/:index',validation, util.fileSend)
 // Route which return the another file with 25000 limit
-router.get('/file2/:index',util.fileSendother)
+router.get('/file2/:index',validation,util.fileSendother)
 
 module.exports=  router;
--- util.js
+++ util.js
@@ -3,43 +3,27 @@
 //file send just locally with checking value of index of route
 exports.fileSend= (req,res)=>{  
     const indexvalue= parseInt(req.params.index)
-    if(indexvalue==0){
-        res.status(401).send('Route value must not be equal to 0')
-    }
-    console.log(indexvalue===isNaN())
-    if(indexvalue===isNaN()){
-        res.status(500).send(`index is not a number in url last`)
         
-        
-    }else{
-        if(indexvalue<=50000){
+    if(indexvalue<=50000){
             res.sendFile(__dirname+"/route/book.json",(err,data)=>{
                 if(err){console.log(err)}
                 console.log('Sent','book.json')
             })
         }else{
-            res.status(500).send('Route exceeds the value of 50000')
-        }    
-    }  
+            res.status(500).send('Route have string value in last')
+        }      
 }
 //Another books send if route file is in 25000
 exports.fileSendother= (req,res)=>{  
     const indexvalue2= parseInt(req.params.index)
-    if(indexvalue2==0){
-        res.status(401).send('Route value must not be equal to 0')
-        return 0;
-    }
-    console.log(indexvalue2===isNaN())
-    if(indexvalue2===isNaN()){
-        res.status(500).send('index is not a number of url last')
-    }else{
-        if(indexvalue2<=50000){
+        
+    if(indexvalue2<=50000){
             res.sendFile(__dirname+"/route/book2.json",(err,data)=>{
                 if(err){console.log(err)}
                 console.log('Sent','book2.json')
             })
         }else{
-            res.status(500).send('Route exceeds the value of 50000')
+            res.status(500).send('Route have string value in last')
         }    
-    }      
+          
 }
